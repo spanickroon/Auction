d2b92aab74667461c23ae93b40fc9faeb3a3eb9c
Nikita Koznev <nikitakoznev@gmail.com>
2019-12-24 18:24:51 +0300

    Create project

---
+++ Auction/Auction/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Auction.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
